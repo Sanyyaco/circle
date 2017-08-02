9d1b9a6d30a91350ee6f53a05878887150ebd36f
Administrator <sanyyako@gmail.com>
2017-08-02 17:11:08 +0500

    multithreading: корректировка работы нитей и функционал сравнение массивов

--- src/ru/geekbrains/mulithreading/MyThread.java
+++ src/ru/geekbrains/mulithreading/MyThread.java
@@ -1,24 +1,32 @@
 package ru.geekbrains.mulithreading;
 
 import static ru.geekbrains.mulithreading.Solution.h;
+import static ru.geekbrains.mulithreading.Solution.size;
 
 /**
  * Created by Administrator on 02.08.2017.
  */
 public class MyThread extends Thread {
 
-    float[] arr = new float[h];
+    private float[] arr;
+    private int size;
+    private int offset;
+    private long a;
 
-    public MyThread(float[] arr) {
+    public MyThread(float[] arr, int size, int offset) {
         this.arr = arr;
+        this.size = size;
+        this.offset = offset;
         System.out.println(this.getName()+ ": начал подсчет.");
         start();
+        a = System.currentTimeMillis();
     }
 
     @Override
     public void run() {
-        for (int i = 0; i < h ; i++) {
-            arr[i] = (float)(arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+        for (int i = 0; i < arr.length ; i++) {
+            arr[i] = (float)(arr[i] * Math.sin(0.2f + (i+offset) / 5) * Math.cos(0.2f + (i+offset) / 5) * Math.cos(0.4f + (i+offset) / 2));
         }
+        System.out.println(this.getName()+ ": закончил подсчет за " + (System.currentTimeMillis() - a) + " милсек.");
     }
 }
--- src/ru/geekbrains/mulithreading/Solution.java
+++ src/ru/geekbrains/mulithreading/Solution.java
@@ -1,51 +1,63 @@
 package ru.geekbrains.mulithreading;
 
+import java.util.Arrays;
+
 /**
  * Created by Administrator on 31.07.2017.
  */
 public class Solution {
 
-    static final int size = 10000000;
+    static final int size = 10;
     static final int h = size / 2;
+    static float[] arr1 = new float[size];
+    static float[] arr2 = new float[size];
 
     public static void main(String[] args) {
         method1();
         method2();
+
+        if (Arrays.equals(arr1,arr2)) System.out.println("Массивы одинаковы");
+        else System.out.println("Массивы разные");
     }
 
     static void method1(){
 
-        float[] arr = new float[size];
-
         for (int i = 0; i < size ; i++) {
-            arr[i] = 1;
+            arr1[i] = 1;
         }
 
         long a = System.currentTimeMillis();
         for (int i = 0; i < size ; i++) {
-            arr[i] = (float)(arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+            arr1[i] = (float)(arr1[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
         }
         System.out.println(System.currentTimeMillis() - a);
     }
 
     static void method2(){
-        float[] arr = new float[size];
 
         for (int i = 0; i < size ; i++) {
-            arr[i] = 1;
+            arr2[i] = 1;
         }
         long a = System.currentTimeMillis();
         float[] a1 = new float[h];
         float[] a2 = new float[h];
 
-        System.arraycopy(arr, 0, a1, 0, h);
-        System.arraycopy(arr, h, a2, 0, h);
+        System.arraycopy(arr2, 0, a1, 0, h);
+        System.arraycopy(arr2, h, a2, 0, h);
 
-        MyThread myThread = new MyThread(a1);
-        MyThread myThread2 = new MyThread(a2);
+        MyThread myThread = new MyThread(a1, h,0);
+        MyThread myThread2 = new MyThread(a2, h,h);
 
-        System.arraycopy(a1, 0, arr, 0, h);
-        System.arraycopy(a2, 0, arr, h, h);
+
+        try {
+            myThread.join();
+            myThread2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.arraycopy(a1, 0, arr2, 0, h);
+        System.arraycopy(a2, 0, arr2, h, h);
 
         System.out.println(System.currentTimeMillis() - a);
     }
